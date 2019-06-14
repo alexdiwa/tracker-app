90274943d748a7b7915f549cdd76e239f68960c3
Alex <atdiwa@gmail.com>
2019-06-14 16:38:01 +1000

    added views for creating job, controller function and routes

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -5,6 +5,20 @@ async function index(req, res) {
   res.render("job/index", {jobs})
 }
 
+async function create(req, res) {
+  let {title, company, description, status, outcome} = req.body;
+  let job = await JobModel.create({ title, company, description, status, outcome })
+    .catch(err => res.status(500).send(err));
+
+  res.redirect("/jobs");
+}
+
+async function make(req, res) {
+  res.render("job/new")
+}
+
 module.exports = {
-  index
+  index,
+  create,
+  make
 }
--- routes/job_routes.js
+++ routes/job_routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const JobController = require("./../controllers/job_controller");
 
 router.get("/", JobController.index);
+router.post("/", JobController.create);
+router.get("/new", JobController.make);
 
 module.exports = router;
