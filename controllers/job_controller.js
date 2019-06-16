e07ea70ece0578a6364bf21ffc71ea95e9a7bb1f
Alex <atdiwa@gmail.com>
2019-06-16 21:46:58 +1000

    add show for job listing and functions to view and delete

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -5,20 +5,35 @@ async function index(req, res) {
   res.render("job/index", {jobs})
 }
 
+async function make(req, res) {
+  res.render("job/new")
+}
+
 async function create(req, res) {
   let {title, company, description, status, outcome} = req.body;
-  let job = await JobModel.create({ title, company, description, status, outcome })
+  await JobModel.create({ title, company, description, status, outcome })
     .catch(err => res.status(500).send(err));
 
   res.redirect("/jobs");
 }
 
-async function make(req, res) {
-  res.render("job/new")
+async function show(req, res) {
+  let { id } = req.params;
+  let job = await JobModel.findById(id);
+  res.render("job/show", { job });
 }
+
+async function destroy(req, res) {
+  let { id } = req.params;
+  await JobModel.findByIdAndRemove(id);
+  res.redirect("/jobs");
+}
+
 
 module.exports = {
   index,
   create,
-  make
+  make,
+  show,
+  destroy
 }
--- routes/job_routes.js
+++ routes/job_routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const JobController = require("./../controllers/job_controller");
 
 router.get("/", JobController.index);
-router.post("/", JobController.create);
 router.get("/new", JobController.make);
+router.post("/", JobController.create);
+router.get("/:id", JobController.show);
+router.delete("/:id", JobController.destroy);
 
 module.exports = router;
